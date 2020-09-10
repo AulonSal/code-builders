5bd8994e360a2defcb12876f91f51566ec3b8a4b
Udbhav18 <udbhavsomani18@gmail.com>
2020-09-10 22:49:54 +0530

    _

--- website/home/views.py
+++ website/home/views.py
@@ -7,15 +7,19 @@ from .models import Announcement
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.decorators.clickjacking import xframe_options_sameorigin
 
+
 def home(request):
     return render(request, 'home/home.html')
+
 
 def contact_us(request):
     return render(request, 'home/contact-us.html')
 
+
 @xframe_options_sameorigin
 def contact_frame(request):
     return render(request, 'home/contact-template.html')
+
 
 @login_required
 def dashboard(request):
