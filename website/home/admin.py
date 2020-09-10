a50e4a14e4843cc2f0443da78c1920ed59109fcb
Udbhav18 <udbhavsomani18@gmail.com>
2020-09-11 00:18:52 +0530

    field names in admin panel and tweaks

--- website/home/admin.py
+++ website/home/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 from .models import TeamMember, Participant, Announcement, EventCategory, Event
 
+
+class EventAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_on',)
+
+
 admin.site.register(TeamMember)
 admin.site.register(Participant)
 admin.site.register(Announcement)
 admin.site.register(EventCategory)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
--- website/home/models.py
+++ website/home/models.py
@@ -7,11 +7,17 @@ class TeamMember(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     referral_code = models.CharField(max_length=16)
 
+    def __str__(self):
+        return f"{self.user.first_name}"
+
 
 class Participant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     referrer = models.ForeignKey(TeamMember, on_delete=models.SET_NULL, null=True, blank=True)
     contact_number = PhoneNumberField()
+
+    def __str__(self):
+        return f"{self.user.first_name}"
 
 
 class Announcement(models.Model):
@@ -19,9 +25,19 @@ class Announcement(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.title}"
+
 
 class EventCategory(models.Model):
     type = models.CharField(max_length=80)
+
+    class Meta:
+        verbose_name = 'Event Category'
+        verbose_name_plural = 'Event Categories'
+
+    def __str__(self):
+        return f"{self.type}"
 
 
 class Event(models.Model):
@@ -30,3 +46,10 @@ class Event(models.Model):
     date = models.DateField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(EventCategory, on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
+
+    def __str__(self):
+        return f"{self.title}, {self.category}"
