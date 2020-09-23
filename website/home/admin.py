86e15bdc024919d4ea45e22d6d10c14855c50683
AulonSal <aulonsal@gmail.com>
2020-09-23 23:24:35 +0530

    Better User admin

--- website/home/admin.py
+++ website/home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import TeamMember, Participant, Announcement, EventCategory, Event
+from .models import TeamMember, Participant, Announcement, EventCategory, Event, User
 
 
 class EventAdmin(admin.ModelAdmin):
@@ -10,6 +10,13 @@ class EventAdmin(admin.ModelAdmin):
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ('user', 'paid', 'contact_number', 'referrer', 'order_id')
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'date_joined')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(TeamMember)
 admin.site.register(Participant, ParticipantAdmin)
